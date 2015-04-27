54793f864a81c5d20f7c55e43f0b0770bd0bd435
harry <Administrator@Harrys>
2015-04-28 01:09:20 +0800

    又改了

--- src/aaa/test/FirstGitClass.java
+++ src/aaa/test/FirstGitClass.java
@@ -5,6 +5,7 @@ public class FirstGitClass {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Modified!!!");
+		System.out.println("Modified again!!!");
 	}
 
 }
