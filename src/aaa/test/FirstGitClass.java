d176976a1b2e8f0aff757bd96689a93903275f1f
harry <Administrator@Harrys>
2015-04-28 01:08:19 +0800

    change class name

--- src/aaa/test/FirstGitClass.java
+++ src/aaa/test/FirstGitClass.java
@@ -1,10 +1,10 @@
 package aaa.test;
 
-public class FitGitClass {
+public class FirstGitClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-
+		System.out.println("Modified!!!");
 	}
 
 }
