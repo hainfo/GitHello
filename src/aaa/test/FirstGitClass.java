e64552be637f006dd0bafc6bfc249c19456ab287
Harry Wang <tsuyoi18@163.com>
2018-04-26 22:34:19 +0800

    Revert "master commet"
    This reverts commit 3b3f8d8671da30d22c620a1783f521fe3e0299cf.

--- src/aaa/test/FirstGitClass.java
+++ src/aaa/test/FirstGitClass.java
@@ -3,7 +3,6 @@ package aaa.test;
 public class FirstGitClass {
 
 	public static void main(String[] args) {
-		// hotfix
 		System.out.println("Hello World");
 		System.out.println("Modified!!!");
 		System.out.println("Modified again!!!");
