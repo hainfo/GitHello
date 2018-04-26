ed8261204e79508892cf120c03e406aed8d8f2e2
Harry Wang <tsuyoi18@163.com>
2018-04-26 22:08:24 +0800

    done

--- src/aaa/test/FirstGitClass.java
+++ src/aaa/test/FirstGitClass.java
@@ -6,7 +6,7 @@ public class FirstGitClass {
 		System.out.println("Hello World");
 		System.out.println("Modified!!!");
 		System.out.println("Modified again!!!");
-		System.out.println("Modified @2019!!!");
+		System.out.println("Modified @2018!!!");
 	}
 
 }
